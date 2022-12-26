85817c5f73fc95c3fa4d3435da91fd3733c447e4
olawale-o <omoogunolawale@gmail.com>
2022-12-26 09:04:27 +0100

    Add register service

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,3 +4,8 @@ export const loginService = async (body) => {
   const data = await post('http://localhost:4000/api/v1/users/login', body);
   return data;
 };
+
+export const registerService = async (body) => {
+  const data = await post('http://localhost:4000/api/v1/users/register', body);
+  return data;
+};
